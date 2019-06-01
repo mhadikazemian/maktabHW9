4a49d143456ea7a21fa5e7cca712f264f40697a6
kazemian <mkzn94@gmail.com>
2019-06-01 13:48:36 +0430

    Adds BaseDAO interface

---
+++ src/main/java/com/maktab/core/dao/BaseDAO.java
@@ -0,0 +1,14 @@
+package com.maktab.core.dao;
+
+import java.io.Serializable;
+
+public interface BaseDAO<E extends Serializable> {
+
+    void create(E e);
+
+    E update(E e);
+
+    E read(Serializable id);
+
+    void delete(Serializable id);
+}//end of BaseDAO interface
